53127df44e2622bdca1ce5953364452086014cf4
developerinshafa <zuhrzaidha@gmail.com>
2026-02-18 17:37:31 +0530

    cange the text

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     <Section/>
 
-    <h2 className="text-3xl p-2 font-bold bg-amber-100 ">Here are the top 10 places to visit in Sri Lanka:</h2>
+    <h2 className="text-3xl p-2 font-bold bg-amber-100 ">Here are the 6 places to visit in Sri Lanka:</h2>
     <ul className="text-xl grid gap-2 bg-amber-100 font-semibold ">
 
       {/* <ListItem place="Sigiriya Rock Fortress"/>
