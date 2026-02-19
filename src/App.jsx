7062bdba25d7ffc51e6f1acee1ecf09d52893694
developerinshafa <zuhrzaidha@gmail.com>
2026-02-19 10:37:28 +0530

    image name canges

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
     <Section/>
 
-    <h2 className="text-3xl p-2 font-bold bg-amber-100 ">Here are the 6 places to visit in Sri Lanka:</h2>
+    <h2 className="text-3xl p-2 font-bold bg-amber-100 "> Explore Top Destination:</h2>
     <ul className="text-xl grid gap-2 bg-amber-100 font-semibold ">
 
       {/* <ListItem place="Sigiriya Rock Fortress"/>
@@ -26,12 +26,12 @@ function App() {
       <ListItem place ="Ella"/>
       <ListItem place ="Dambulla Cave Temple"/> */}
      
-      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Sigiriya Rock Fortress</li>
-      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Kandy</li>
-      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Ella</li>
-      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Galle Fort</li>
-      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Nuwara Eliya</li>
-      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Dambulla Cave Temple</li>
+      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Dubai</li>
+      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Sri Lanka</li>
+      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Thailand</li>
+      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Malaysia</li>
+      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Singapore</li>
+      <li className='bg-amber-900 text-white hover:bg-amber-400 space-x-2 p-2'>Bangkok</li>
     </ul>
 
     <Content/>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,19 @@
 const Section = () => {
     return [
-        <section className="flex p-4 bg-amber-50">
-            <h1 className='text-5xl font-bold p-2 text-left'>
+        <section className="flex p-4 gap-10 bg-amber-50 ">
+            <h1 className='text-5xl font-bold p-2 text-left ml-20'>
                 Travel Around <br /> The World
                 <img src="./public/bg (2).png" alt="Travel image" className="img"/>
             </h1>
-            <p className="w-200 text-xl p-4">Sri Lanka is a highly recommended travel destination, offering a 
-                diverse mix of beaches, ancient history, wildlife, and scenic mountains. 
-                Here are some of the top places to visit in Sri Lanka based on your interests.
+            <p className="w-150 text-xl p-4 text-justify">
+            Escape the ordinary and dive into unforgettable adventures. 
+            Whether you crave tranquil beaches, breathtaking mountain escapes, 
+            or vibrant city explorations, we craft experiences that match your dreams. 
+            Travel isn’t just about the destination—it’s about the memories you create 
+            along the way. With our carefully curated packages, seamless itineraries, 
+            and expert guidance, you can embrace new cultures, discover hidden gems, 
+            and travel with complete peace of mind. Let us handle the details while you 
+            focus on making moments that last a lifetime.
             </p>
         </section>
 
